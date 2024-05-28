4d26031610e7d9791e058080401381bcdf198ddc
Ferdie De Oliveira <deferdie@gmail.com>
2024-05-28 19:33:21 +0100

    set headers to fetch

--- src/Synthetic.js
+++ src/Synthetic.js
@@ -2,10 +2,20 @@ export default class Synthetic {
     API_ENDPOINT = 'https://app.acumenlogs.com/'
 
     startSynthetic(url) {
-        return fetch(url);
+        return fetch(url, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+            }
+        });
     }
 
     getBatch(syntheticToken, batchId) {
-        return fetch(`${this.API_ENDPOINT}/status/watcher/${syntheticToken}/${batchId}`);
+        return fetch(`${this.API_ENDPOINT}/status/watcher/${syntheticToken}/${batchId}`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+            }
+        });
     }
 }
