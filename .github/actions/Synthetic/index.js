4e21d0ea26e83011eda6e45b5afd70ff3f59ac1a
Ferdie De Oliveira <deferdie@gmail.com>
2024-05-28 18:30:50 +0100

    Synthetic test init

--- .github/actions/Synthetic/index.js
+++ .github/actions/Synthetic/index.js
@@ -7,7 +7,30 @@ const {
 async function run() {
   const syn = new Synthetic();
 
-  syn.startSynthetic(core.getInput('SYNTHETIC_TEST_URL'));
+  const test = await (await syn.startSynthetic(core.getInput('SYNTHETIC_TEST_URL'))).json();
+
+  console.log(test.batch.id);
+
+  const  MAX_RETRIES = 210;
+  let retries = 0;
+
+  await new Promise((resolve, reject) => {
+    setInterval(async () => {
+      const result = await (await syn.getBatch(test.batch.token, test.batch.id)).json();
+      if (result.status === 'completed') {
+        return resolve(result)
+      }
+      
+      if (result.status === 'completed' && (result.has_passed == false || result.has_passed == 0)) {
+        return reject("Failed synthetic test")
+      }
+      
+      retries = retries +1;
+      if (retries >= MAX_RETRIES) {
+        return reject("Synthetic action timeout")
+      }
+    }, 4000);
+  });
 }
 
 run().catch((error) => {
--- src/Synthetic.js
+++ src/Synthetic.js
@@ -1,5 +1,11 @@
 export default class Synthetic {
+    API_ENDPOINT = 'https://app.acumenlogs.com/'
+
     startSynthetic(url) {
-        console.log(url)
+        return fetch(url);
+    }
+
+    getBatch(syntheticToken, batchId) {
+        return fetch(`${API_ENDPOINT}/status/watcher/${syntheticToken}/${batchId}`);
     }
 }
